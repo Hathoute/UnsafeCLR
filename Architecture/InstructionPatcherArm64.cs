06f5d0424c8630f1083aac96fd1288dcd0b86549
Hamza HATHOUTE <hamza.hathoute.e@thalesdigital.io>
2025-04-17 12:38:56 +0200

    feat: attempt to support net6.0 on ARM64

--- Architecture/InstructionPatcherArm64.cs
+++ Architecture/InstructionPatcherArm64.cs
@@ -1,35 +1,65 @@
+using System.Diagnostics;
+
 namespace UnsafeCLR.Architecture;
 
 internal unsafe class InstructionPatcherArm64 : IInstructionPatcher {
 
     // https://developer.arm.com/documentation/ddi0602/2025-03/Base-Instructions/LDR--literal---Load-register--literal--
     // Only supporting the 64-bit variant of the instruction
-    private const int Arm64LdrLiteralSignature = 0b01011000 << 24;
+    private const int Arm64LdrLiteralSignature = 0b01011000;
+    
+    private const int Arm64AdrSignature =    0b00010000;
+    private const int Arm64AdrSignatureXor = 0b01100000;
     
     public IntPtr FindJumpAbsoluteAddress(IntPtr jmpInstruction) {
-        var instruction = UnsafeOperations.Read<int>(jmpInstruction);
+        var currentInstructionPtr = jmpInstruction;
+        SkipAdrInstruction(ref currentInstructionPtr);
+        
+        var instruction = UnsafeOperations.Read<int>(currentInstructionPtr);
         if (!IsLdrLiteralArm64Instruction(instruction, out var imm19, out _)) {
             throw new ArgumentException("jmpInstruction does not point to a LDR instruction");
         }
 
         var displacement = imm19 * 0x4;
-        var addrPtr = (IntPtr*) IntPtr.Add(jmpInstruction, displacement);
+        var addrPtr = (IntPtr*) IntPtr.Add(currentInstructionPtr, displacement);
         return *addrPtr;
     }
 
     public void PatchJumpWithAbsoluteAddress(IntPtr jmpInstruction, IntPtr absoluteAddress) {
-        var instruction = UnsafeOperations.Read<int>(jmpInstruction);
+        var currentInstructionPtr = jmpInstruction;
+        SkipAdrInstruction(ref currentInstructionPtr);
+        
+        var instruction = UnsafeOperations.Read<int>(currentInstructionPtr);
         if (!IsLdrLiteralArm64Instruction(instruction, out var imm19, out _)) {
             throw new ArgumentException("jmpInstruction does not point to a LDR instruction");
         }
         
         var displacement = imm19 * 0x4;
-        var addrPtr = (IntPtr*) IntPtr.Add(jmpInstruction, displacement);
+        var addrPtr = (IntPtr*) IntPtr.Add(currentInstructionPtr, displacement);
+        Program.Main((ulong) ((IntPtr) addrPtr).ToInt64());
         *addrPtr = absoluteAddress;
+    }
+
+    private static void SkipAdrInstruction(ref IntPtr jmpInstructionPtr) {
+        // In net6.0, the first instruction is 'adr X(Rd), #0', which will load PC into the register Rd
+        //  followed by a ldr and then br (just like net7.0)
+        var instruction = UnsafeOperations.Read<int>(jmpInstructionPtr);
+        if (!IsAdrArm64Instruction(instruction, out var imm19, out var rd)) {
+            return;
+        }
+
+        // Some assertions, since I'm not yet sure if its consistent
+        // TODO: confirm this by looking at the CoreCLR (precisely JIT code)
+        Debug.Assert(imm19 == 0);
+        Debug.Assert(rd == 12);
+            
+        // Skip this instruction
+        jmpInstructionPtr = IntPtr.Add(jmpInstructionPtr, 4);
     }
     
     private static bool IsLdrLiteralArm64Instruction(int instruction, out int imm19, out int rt) {
-        if (!HasSameSignature(instruction, Arm64LdrLiteralSignature, 0xFF << 24)) {
+        var signature = instruction >> 24;
+        if ((signature ^ Arm64LdrLiteralSignature) != 0) {
             imm19 = 0;
             rt = 0;
             return false;
@@ -39,8 +69,17 @@ internal unsafe class InstructionPatcherArm64 : IInstructionPatcher {
         rt = instruction & 0b11111;
         return true;
     }
-    
-    private static bool HasSameSignature(int instruction, int signature, int mask) {
-        return ((instruction ^ signature) & mask) == 0;
+
+    private static bool IsAdrArm64Instruction(int instruction, out int imm, out int rd) {
+        var signature = instruction >> 24;
+        if (((signature ^ Arm64AdrSignature) | Arm64AdrSignatureXor) != Arm64AdrSignatureXor) {
+            imm = 0;
+            rd = 0;
+            return false;
+        }
+
+        imm = (instruction >> 2) & 0xFFFFFC + (instruction >> 29) & 0b11;
+        rd = instruction & 0b11111;
+        return true;
     }
 }
